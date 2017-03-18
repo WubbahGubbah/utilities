d3b1cd7f8dfcd6d89eeb9144eba42a3d763da621
Matthew Wilson <wubbahgubbah@gmail.com>
2017-03-18 08:42:50 -0400

    update nodefile1.js
    added new file path, but need to look into why copy isn't working, only zero byte file output

--- nodefile1.js
+++ nodefile1.js
@@ -17,14 +17,14 @@ fs.readdir(initial, function( err, files) {
 
     files.forEach( function(file,index){
         //are the tags the same for each image type?
-        if (path.extname(file.toLowerCase()) == ".jpg" || path.extname(file.toLowerCase()) == ".png") {
+        if (path.extname(file.toLowerCase()) == ".jpg" || path.extname(file.toLowerCase()) == ".png" || path.extname(file.toLowerCase()) == ".mov") {
         console.log(file);
         var stats = fs.statSync(initial + file.toString());
         var mtime = new Date(util.inspect(stats.mtime));
         var formattedDate =mtime.getFullYear() + ("0"+(mtime.getMonth()+1)).slice(-2) + ("0" + mtime.getDate()).slice(-2);
 
         //console.log(mtime);
-        console.log(formattedDate);
+        //console.log(formattedDate);
 
         //next steps
         //check for folder existence
@@ -33,7 +33,12 @@ fs.readdir(initial, function( err, files) {
         }
 
         //copy file from one folder to backup folder
-        //add folder to copied collection
+        newFileName = path.join(targetRoot + formattedDate + '/' + path.basename(file));
+        //this isn't working, files are being copied as zero bytes
+        fs.createReadStream(file).pipe(fs.createWriteStream(newFileName));
+        console.log(newFileName);
+        //add file to copied collection
+        //copiedFiles.add(newFileName);
         //in the end, loop through the copied collection and verify existence before deleting
 
       }
